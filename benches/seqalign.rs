36348e2d24679e24ddee409971e0a0d923fb9e3f
Daniël de Kok <me@danieldk.eu>
2018-03-18 12:06:26 +0100

    Add LCS and Levenshtein-Damerau distance benchmakrs.

--- benches/seqalign.rs
+++ benches/seqalign.rs
@@ -1,46 +1,69 @@
 #![feature(test)]
 
 extern crate rand;
-extern crate test;
 extern crate seqalign;
+extern crate test;
 
 use test::Bencher;
 
-use rand::{Rng, weak_rng};
-use seqalign::Align;
-use seqalign::measures::Levenshtein;
+use rand::{weak_rng, Rng};
+use seqalign::{Align, Measure};
+use seqalign::measures::{Levenshtein, LevenshteinDamerau, LCS};
 
 static BENCH_ALPHABET: &[char] = &['a', 'b', 'c', 'd', 'e'];
 
-fn random_string<R>(rng: &mut R, len: usize) -> Vec<char> where R: Rng {
-	(0..len).map(|_| rng.choose(BENCH_ALPHABET).unwrap()).cloned().collect()
+fn random_string<R>(rng: &mut R, len: usize) -> Vec<char>
+where
+    R: Rng,
+{
+    (0..len)
+        .map(|_| rng.choose(BENCH_ALPHABET).unwrap())
+        .cloned()
+        .collect()
 }
 
-fn random_pair<R>(rng: &mut R) -> (Vec<char>, Vec<char>) where R: Rng {
-	let s1_len = rng.gen_range(0, 20);
-	let s2_len = rng.gen_range(0, 20);
+fn random_pair<R>(rng: &mut R) -> (Vec<char>, Vec<char>)
+where
+    R: Rng,
+{
+    let s1_len = rng.gen_range(0, 20);
+    let s2_len = rng.gen_range(0, 20);
 
-	let s1 = random_string(rng, s1_len);
-	let s2 = random_string(rng, s2_len);
+    let s1 = random_string(rng, s1_len);
+    let s2 = random_string(rng, s2_len);
 
-	(s1, s2)
+    (s1, s2)
+}
+
+fn distance_bench<M>(b: &mut Bencher, measure: M)
+where
+    M: Measure<char>,
+{
+    let mut rng = weak_rng();
+
+    let mut pairs = Vec::new();
+    for _ in 0..1000 {
+        pairs.push(random_pair(&mut rng));
+    }
+
+    b.iter(move || {
+        for &(ref s1, ref s2) in &pairs {
+            measure.align(s1, s2);
+        }
+    })
+}
+
+#[bench]
+fn lcs_distance_1000(b: &mut Bencher) {
+    distance_bench(b, LCS::new(1, 1));
+}
+
+#[bench]
+fn levenshtein_damerau_distance_1000(b: &mut Bencher) {
+    distance_bench(b, LevenshteinDamerau::new(1, 1, 1, 1));
 }
 
 #[bench]
 fn levenshtein_distance_1000(b: &mut Bencher) {
-	let mut rng = weak_rng();
-
-	let mut pairs = Vec::new();
-	for _ in 0..1000 {
-		pairs.push(random_pair(&mut rng));
-
-	}
-
-	let levensthein = Levenshtein::new(1, 1, 1);
-
-	b.iter(move || {
-		for &(ref s1, ref s2) in &pairs {
-			levensthein.align(s1, s2);
-		}
-    })
+    distance_bench(b, Levenshtein::new(1, 1, 1));
 }
